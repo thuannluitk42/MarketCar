92dd5af51b4c259e57a368664aabe18b50050765
ThuanLeMerkle <thuan.le@merkle.com>
2023-04-08 18:05:53 +0700

    no bug

--- dbConnect.js
+++ dbConnect.js
@@ -4,12 +4,10 @@ const mongoose = require("mongoose");
 const dbConnect = () => {
 
 	// Connect to your MongoDB database
-    const connectionParams = { useNewUrlParser: true };
+    const connectionParams = { useNewUrlParser: true,useUnifiedTopology: true };
     mongoose.connect(process.env.MONGODB_URI, connectionParams);
 	mongoose.set("strictQuery",false);
-	// mongoose.connect("mongodb+srv://admin:axusDfXXWOAg7wW4@cluster0.jvixxfw.mongodb.net/MarketCar", connectionParams);
-	// mongoose.set("strictQuery",false);
-	// mongoose.connect("mongodb+srv://admin:axusDfXXWOAg7wW4@cluster0.jvixxfw.mongodb.net/MarketCar");
+
 
     mongoose.connection.on("connected", () => {
 		console.log("Connected to database sucessfully");
